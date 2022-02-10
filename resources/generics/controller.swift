d8dc6ba8c1a1f6a1226ca1e9773edd5e680cfb1e
hameed <waelalmosawi4@gmail.com>
2022-02-10 10:32:43 +0300

    update background color

--- resources/generics/controller.swift
+++ resources/generics/controller.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 import UIKit
+import SnapKit
+
+protocol controllerFunctions:AnyObject {
+    func setUPUI()
+    func updateView()
+    func viewBinder()
+}
 
 class controller:UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = MAIN_APP_COLOR
     }
 }
