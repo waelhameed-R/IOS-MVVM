87dfa6a14f7a7f05797596329635f2b4a4434a6f
hameed <waelalmosawi4@gmail.com>
2022-02-10 10:32:17 +0300

    set constant

---
+++ resources/constant/constant.swift
@@ -0,0 +1,15 @@
+//
+//  constant.swift
+//  app
+//
+//  Created by wael hameed on 2/10/22.
+//
+
+import Foundation
+import UIKit
+
+// MARK:- Color -
+public let MAIN_APP_COLOR : UIColor = UIColor(named: "mainAppColor")!
+
+// MARK:- Images
+public let APP_LOGO:UIImage = UIImage(named: "logo")!
